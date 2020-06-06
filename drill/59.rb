65a0bcc8927c33f48b46fb76eb2c0c0cd56c3acf
naoto-ysd <na_oto1111@yahoo.co.jp>
2020-06-06 23:25:26 +0900

    Add 59.rb

---
+++ drill/59.rb
@@ -0,0 +1,30 @@
+# あなたは警官です。aとｂ二人の容疑者の取り調べをしています。
+# どちらも証言がTrue、またはFalseであればその証言はTrueです。
+# しかしどちらかがFalseでTrueであればその証言はFalse、と出力するメソッドを作りましょう。
+
+# 呼び出し方：
+# police_trouble(a, b)
+
+# 出力例：
+# police_trouble(true, false) → False
+# police_trouble(false, false) → True
+# police_trouble(true, true) → True
+
+# 模範解答
+# def police_trouble(a, b)
+#   if a && b || !a && !b
+#     puts "True"
+#   else
+#     puts "False"
+#   end
+# end
+
+def police_trouble(a, b)
+  if a == b
+    p true
+  else
+    p false
+  end
+end
+
+police_trouble(false, false)
