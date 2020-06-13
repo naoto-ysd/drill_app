c7fb6577de7fd1ded04c81b6dd29a990e7dcf6da
naoto-ysd <na_oto1111@yahoo.co.jp>
2020-06-13 23:53:06 +0900

    Create 48.rb

---
+++ drill/48.rb
@@ -0,0 +1,12 @@
+# class Impression < ActiveRecord::Base
+#   VALID_YOUTUBE_URL_REGEX = ???
+#   validates :url, presence: true, format: { with: VALID_YOUTUBE_URL_REGEX }
+# end
+# 2行目のvalidationを完成させてください。
+# ただし条件は、「youtube」という文字が投稿された文字列に含まれていることを確かめることとします。
+
+VALID_YOUTUBE_URL_REGEX = /\A.*youtube.*\z/
+validates :text, presence: true, format: { with: VALID_YOUTUBE_URL_REGEX }
+# （補足）
+# 正規表現を使ってバリデーションを作成する簡単なサンプルです。
+# \Aや\zをつける理由や「.*」の意味を確認しておきましょう。
