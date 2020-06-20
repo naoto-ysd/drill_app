bbadf8732c2c35c372df4bcb2ba860eba117529b
naoto-ysd <na_oto1111@yahoo.co.jp>
2020-06-20 23:48:02 +0900

    Add 42.rb

--- drill/42.rb
+++ drill/42.rb
@@ -1,1 +1,21 @@
-itemsコントローラのアクションが呼び出された際の、レイアウトファイルとしてapp/views/layouts/hogehoge.htmlが使われるようになる。何も指定しない場合はapp/views/layouts/application.htmlが読み込まれる。
+# Railsで、定期的に実行したい処理は、lib/tasks以下のディレクトリに、rakeタスクを作成して処理を記述することが多いです。
+# 次のコードは、全てのユーザーに対して、ユーザーが入店したかどうかを管理する
+# "enterd_flag"というカラムをfalseにするrakeタスクです。
+# rakeタスクの使い方を確認した上で、下記のrakeタスクを
+# ターミナルから実行するためには、どのようなコマンドを実行すればいいか、答えてください。
+
+# lib/tasks/user_checker.rb
+namespace :user do
+  desc "ユーザーの入店情報をリセットする"
+  task reset_entered_flag: :environment do
+    User.update_all(entered_flag: false)
+  end
+end
+
+
+# 模範回答
+# rake user:reset_entered_flag
+# rake [namespeceの名前]:[taskの名前]のように記述して実行します。
+# 毎日・毎週特定の曜日・３日おきなど、特定の周期でrakeタスクを自動実行したい場合は、
+# wheneverというgemを組み合わせて利用します。興味のある方は、是非下記リンクを参考にしてみてください。
+# https://morizyun.github.io/blog/whenever-gem-rails-ruby-capistrano/index.html
--- drill/43.rb
+++ drill/43.rb
@@ -13,6 +13,8 @@ end
 # コントローラによってヘッダー・フッターのデザインを変更したり、読み込むCSSを切り替えるメソッド。
 
 
-# ②①のコードで、itemsコントローラに「layout 'hogehoge'」があることで、ない場合と具体的にどのような動作の違いが出るか説明してください。
+# ②①のコードで、itemsコントローラに「layout 'hogehoge'」があることで、
+# ない場合と具体的にどのような動作の違いが出るか説明してください。
 # 回答
-# itemsコントローラのアクションが呼び出された際の、レイアウトファイルとしてapp/views/layouts/hogehoge.htmlが使われるようになる。何も指定しない場合はapp/views/layouts/application.htmlが読み込まれる。
+# itemsコントローラのアクションが呼び出された際の、
+# レイアウトファイルとしてapp/views/layouts/hogehoge.htmlが使われるようになる。何も指定しない場合はapp/views/layouts/application.htmlが読み込まれる。
